3537c2e31f6771f3c1b4e19ee8942c268198b743
Tadhg N <tadhgnolan@gmail.com>
2024-02-19 18:39:11 +0000

    Add name to workout list in generator admin

--- generator/admin.py
+++ generator/admin.py
@@ -4,9 +4,9 @@ from .models import Workout, Exercise
 
 @admin.register(Workout)
 class WorkoutAdmin(admin.ModelAdmin):
-    list_display = ('body_part',)
+    list_display = ('name', 'body_part')
 
 
 @admin.register(Exercise)
 class ExerciseAdmin(admin.ModelAdmin):
-    list_display = ('name', 'muscle_focus', 'sets', 'reps')
+    list_display = ('name', 'workout', 'muscle_focus', 'sets', 'reps')
