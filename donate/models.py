670410744d5f74b79b026dff5bf677780eba6921
Tadhg N <tadhgnolan@gmail.com>
2024-02-22 11:31:13 +0000

    Update donate model

--- donate/models.py
+++ donate/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
 class Donation(models.Model):
-    amount = models.DecimalField(max_digits=6, decimal_places=2)
+    name = models.CharField(max_length=2555, unique=True)
+    amount = models.DecimalField(max_digits=6, decimal_places=2, default=5.00) # €5 fixed amount
+    stripe_product_id = models.CharField(max_length=255, unique=True) # Store Stripe Product ID
     date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
