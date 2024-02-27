ede5907639bb5adb9402516fafec3bbfe0abb53c
Tadhg N <tadhgnolan@gmail.com>
2024-02-27 21:23:02 +0000

    Correct indentation error line 119 in settings

--- workout_generator/settings.py
+++ workout_generator/settings.py
@@ -113,9 +113,9 @@ WSGI_APPLICATION = 'workout_generator.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 if 'DATABASE_URL' in os.environ:
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 else:
     DATABASES = {
         'default': {
