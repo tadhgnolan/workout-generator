d3168aee359b62cab74ed85e173f8c511cbd161e
Tadhg N <tadhgnolan@gmail.com>
2024-02-28 04:05:04 +0000

    Comment out unused code in donate urls

--- donate/urls.py
+++ donate/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.donation, name='donation'),
-    path(
-        'cache_donate_data/', views.cache_donate_data, name='cache_donate_data'
-    ),
+    path('', views.donation_view, name='donation_view'),
+    # path(
+    #     'cache_donate_data/', views.cache_donate_data, name='cache_donate_data'
+    # ),
     path(
         'donation_success/<order_number>',
         views.donation_success,
