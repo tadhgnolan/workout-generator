cb04c75cb65f68dc94a333b6a8cbf2e930984cf7
Tadhg N <tadhgnolan@gmail.com>
2024-04-24 23:30:22 +0000

    Add exercise CRUD paths to generator urls

--- generator/urls.py
+++ generator/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('add_workout/', views.add_workout, name='add_workout'),
     path('edit_workout/<int:id>/', views.edit_workout, name='edit_workout'),
     path('delete_workout/<int:id>/', views.delete_workout, name='delete_workout'),
+    path('add_exercise/', views.add_exercise, name='add_exercise'),
+    path('edit_exercise/<int:id>/', views.edit_exercise, name='edit_exercise'),
+    path('delete_exercise/<int:id>/', views.delete_exercise, name='delete_exercise'),
 ]
