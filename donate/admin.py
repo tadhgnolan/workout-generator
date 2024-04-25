11af4a1fdda6170553b132b4a25b402c9b1ce410
Tadhg N <tadhgnolan@gmail.com>
2024-04-25 14:36:13 +0000

    Add admin donation model

--- donate/admin.py
+++ donate/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Donation
 
-# Register your models here.
+
+@admin.register(Donation)
+class DonationAdmin(admin.ModelAdmin):
+    list_display = ('email', 'amount', 'date', 'order_number')
