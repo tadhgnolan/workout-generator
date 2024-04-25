1ef73c5ae0fdc18233af39db2d591c8eb5e43929
Tadhg N <tadhgnolan@gmail.com>
2024-04-25 14:33:38 +0000

    Update donate view for stripe & email functionality

--- donate/views.py
+++ donate/views.py
@@ -1,10 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, HttpResponse
 from django.conf import settings
+from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views.decorators.http import require_POST
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+
 from .forms import DonationForm
 from .models import Donation
+
 import stripe
+
+
+@require_POST
+def cache_donate_data(request):
+    try:
+        amount = request.POST.get('amount')
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'amount': amount,
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(
+            request,
+            'Sorry, your payment cannot be processed right now. '
+            'Please try again later.'
+        )
+        return HttpResponse(content=e, status=400)
 
 
 def donation_view(request):
@@ -14,19 +40,13 @@ def donation_view(request):
     if request.method == 'POST':
         form = DonationForm(request.POST)
         if form.is_valid():
-            amount = form.cleaned_data['amount']
-            amount_in_cents = int(amount * 100)
-            charge = stripe.Charge.create(
-                amount=amount_in_cents,
-                currency='eur',
-                description='Donation',
-                source=request.POST['stripeToken']
-            )
-            donation = Donation(amount=amount)
+            donation = form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            donation.stripe_product_id = pid
             donation.save()
             return HttpResponseRedirect(reverse('donation_success', args=[donation.order_number]))
     else:
-        stripe_total = round(5 * 100)
+        stripe_total = round(5 * 100)  # TODO: this shouldn't be 5 * 100 (it should be the amount the user donates)
         stripe.api_key = stripe_secret_key
         intent = stripe.PaymentIntent.create(
             amount=stripe_total,
@@ -47,16 +67,31 @@ def donation_success(request, order_number):
     Handle successful donations
     """
     save_info = request.session.get('save_info')
-    order = get_object_or_404(Order, order_number=order_number)
-    messages.success(request, f'Donation successfully processed! \
-        Your donation order number is {order_number}. A confirmation \
-        email will be sent to {order.email}.')
+    order = get_object_or_404(Donation, order_number=order_number)
+    messages.success(
+        request,
+        'Donation successfully processed! '
+        f'Your donation order number is {order.order_number}. '
+        f'A confirmation email will be sent to {order.email}.'
+    )
     
     customer_email = order.email
     contact_email = settings.DEFAULT_FROM_EMAIL
     subject = "Workout Generator Donation Confirmation"
-    body = {
-        "Thank you for donating to Workout Generator. \n"
-        "Please find your donation order details below. \n\n"
-        "Order Number: {order.order_number} \n"
+    body = render_to_string(
+        'donate/confirmation_emails/confirmation_email_body.txt',
+        {'donation': order, 'contact_email': contact_email}
+    )
+    send_mail(
+        subject,
+        body,
+        contact_email,
+        [customer_email]
+    )
+
+    template = 'donate/donation_success.html'
+    context = {
+        'donation': order,
     }
+
+    return render(request, template, context)
