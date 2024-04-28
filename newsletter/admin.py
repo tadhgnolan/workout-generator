64e154783cc4ee60dd18b93629ea47d1a0cbc913
Tadhg N <tadhgnolan@gmail.com>
2024-04-28 18:54:27 +0000

    Register Newsletter app to admin dashboard

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Newsletter
 
-# Register your models here.
+
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = ("email",)
+
+admin.site.register(Newsletter, NewsletterAdmin)
